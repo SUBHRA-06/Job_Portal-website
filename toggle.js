e7a90b87ecdf26ba4cb649a48ea2634cb565ced3
Subhra Sudipta Parida <subhraparida04@gmail.com>
2025-05-19 13:45:38 +0530

    Update toggle.js

--- toggle.js
+++ toggle.js
@@ -1,1 +1,10 @@
 
+const bar = document.getElementById("bar")
+const menu = document.getElementById("menu")
+
+if(bar){
+    bar.addEventListener("click",()=> {
+        menu.classList.toggle("active");
+    });
+
+}
